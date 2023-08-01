fa37262821fa68f50cc29181cfe0bf8cb96d0003
FAISAL HALIM <ffaisalhalim@gmail.com>
2023-08-01 10:00:01 +0700

    fix (kunjungan) : data empty

--- app/Http/Controllers/api/dokter/KunjunganController.php
+++ app/Http/Controllers/api/dokter/KunjunganController.php
@@ -35,10 +35,10 @@ class KunjunganController extends Controller
         return isSuccess($kunjungan, 'Data berhasil dimuat');
     }
 
-    private function getTotal($source) {
+    private function getTotal($source)
+    {   
         $dataMap = ["UMUM", "BPJS", "TOTAL"];
-        $data = $source->get();
-        $data = $data->pluck('penjab')->countBy(
+        $data = $source->pluck('penjab')->countBy(
             function ($item, $key) {
                 return str_contains($item['png_jawab'], "BPJS") ? 'BPJS' : $item['png_jawab'];
             }
@@ -57,41 +57,41 @@ class KunjunganController extends Controller
         return $data;
     }
 
-    function rekap(Request $request) {
-        
+    function rekap(Request $request)
+    {
+
         if (!$request->isMethod('post')) {
             return isFail('Method not allowed');
         }
 
-        $pasien = \App\Models\RegPeriksa::with(['pasien', 'penjab', 'poliklinik'])
+        $pasien = \App\Models\RegPeriksa::with(['pasien', 'penjab'])
             ->where('kd_dokter', $this->payload->get('sub'))
             ->orderBy('tgl_registrasi', 'DESC')
             ->orderBy('jam_reg', 'DESC');
 
-        $operasi  = \App\Models\RegPeriksa::with(['pasien', 'penjab'])
+        $operasi = \App\Models\RegPeriksa::with(['pasien', 'penjab'])
             ->where('kd_dokter', $this->payload->get('sub'))
             ->whereHas('operasi')
             ->orderBy('tgl_registrasi', 'DESC')
             ->orderBy('jam_reg', 'DESC');
 
         $start = date('Y-m-01');
-        $end = date('Y-m-t');
+        $end   = date('Y-m-t');
 
         if ($request->tgl_registrasi) {
             $start = \Illuminate\Support\Carbon::parse($request->tgl_registrasi['start'])->format('Y-m-d');
-            $end = \Illuminate\Support\Carbon::parse($request->tgl_registrasi['end'])->format('Y-m-d');
-    
+            $end   = \Illuminate\Support\Carbon::parse($request->tgl_registrasi['end'])->format('Y-m-d');
+
         }
-        
+
         $pasien->whereBetween('tgl_registrasi', [$start, $end]);
         $operasi->whereBetween('tgl_registrasi', [$start, $end]);
 
-        $data = [
-            'ralan' => $this->getTotal($pasien->where('status_lanjut', ucfirst('ralan'))),
-            'ranap' => $this->getTotal($pasien->where('status_lanjut', ucfirst('ranap'))),
-            'operasi' => $this->getTotal($operasi),
-        ];
-        
+        $data = $pasien->get()->groupBy('status_lanjut')->map(function ($item, $key) {
+            return $this->getTotal(collect($item));
+        });
+        $data['Operasi'] = $this->getTotal($operasi->get());
+
         return isSuccess($data, 'Data berhasil dimuat');
     }
 
@@ -115,7 +115,7 @@ class KunjunganController extends Controller
         }
 
         if ($tahun !== null && $bulan !== null && $tanggal !== null) {
-            $fullDate = $tahun . '-' . $bulan . '-' . $tanggal;
+            $fullDate  = $tahun . '-' . $bulan . '-' . $tanggal;
             $kunjungan = \App\Models\RegPeriksa::where('kd_dokter', $this->payload->get('sub'))
                 ->where('tgl_registrasi', $fullDate)
                 ->orderBy('tgl_registrasi', 'desc')
