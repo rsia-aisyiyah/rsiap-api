4544b17b574cc315d441bd4dc46f20e2bb7d8570
FAISAL HALIM <ffaisalhalim@gmail.com>
2023-08-30 08:57:06 +0700

    :rocket: update (api & git) : some update, check description
    increase trottle for api request,
    ignoring file for git.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -43,7 +43,7 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
 
             // 'throttle:api',                  // <-- default
-            'throttle:70,1',
+            'throttle:240,1',
             
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
@@ -66,5 +66,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'jwt.verify' => \App\Http\Middleware\JwtMiddleware::class,
     ];
 }
