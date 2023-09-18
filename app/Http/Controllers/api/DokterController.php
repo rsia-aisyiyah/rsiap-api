6107de4d433881e1b23a14a4f3f2ec0edaa89353
FAISAL HALIM <ffaisalhalim@gmail.com>
2023-09-18 08:47:02 +0700

    feat (endpoint) : get all active dokcter

--- app/Http/Controllers/api/DokterController.php
+++ app/Http/Controllers/api/DokterController.php
@@ -10,16 +10,11 @@ use Illuminate\Http\Request;
  * */ 
 class DokterController extends Controller
 {
-    protected $payload;
-
-    public function __construct()
-    {
-        $this->payload = auth()->payload();
-    }
-
     public function index()
     {
-        $kd_dokter = $this->payload->get('sub');
+        $payload = auth()->payload();
+        $kd_dokter = $payload->get('sub');
+        
         $dokter = \App\Models\Dokter::with(['pegawai', 'pegawai.kualifikasi_staff_klinis', 'spesialis','pegawai.rsia_email_pegawai'])
             ->where('kd_dokter', $kd_dokter)
             ->first();
@@ -29,9 +24,36 @@ class DokterController extends Controller
 
     public function spesialis()
     {
-        $kd_dokter = $this->payload->get('sub');
+        $payload = auth()->payload();
+        $kd_dokter = $payload->get('sub');
+
         $dokter = \App\Models\Dokter::getSpesialis($kd_dokter);
 
         return isSuccess($dokter, 'Data Spesialis dokter berhasil dimuat');
+    }
+
+    public function getData(Request $request)
+    {
+        $dokter = \App\Models\Dokter::select('kd_sps', 'nm_dokter')
+            ->with(['spesialis'])
+            ->where('status', '1')
+            ->where('nm_dokter', "<>", '-')
+            ->where('kd_dokter', "<>", '-');
+
+        if ($request->has('sps')) {
+            $dokter->where('nm_sps', 'like', '%' . $request->sps . '%');
+        }
+
+        if ($request->has('nm_dokter')) {
+            $dokter->where('nm_dokter', 'like', '%' . $request->nm_dokter . '%');
+        }
+
+        if ($request->has('paginate')) {
+            $dokter = $dokter->paginate($request->paginate);
+        } else {
+            $dokter = $dokter->get();
+        }
+
+        return isSuccess($dokter, 'Dokter berhasil dimuat');
     }
 }
--- routes/partials/api_dokter.php
+++ routes/partials/api_dokter.php
@@ -3,7 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\DokterController;
 
-Route::middleware('api')->prefix('dokter')->group(function ($router) {
+
+Route::prefix('dokter')->group(function ($router) {
+    Route::get('/active/get', [DokterController::class, 'getData']);
+});
+
     Route::get('/', [DokterController::class, 'index']);
     Route::get('spesialis', [DokterController::class, 'spesialis']);
 });
