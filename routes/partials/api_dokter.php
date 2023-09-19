b1c799b432807f9cddf4c49f30440e6ce46b9559
msfiqih <msfiqih@gmail.com>
2023-09-19 09:40:08 +0700

    feat (data) : new endpoint get spesialis of dokter

--- routes/partials/api_dokter.php
+++ routes/partials/api_dokter.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\api\DokterController;
 
 Route::prefix('dokter')->group(function ($router) {
     Route::get('/active/get', [DokterController::class, 'getData']);
+    Route::get('/spesialis/get', [DokterController::class, 'getSpesialis']);
 });
 
 Route::middleware('jwt.verify')->prefix('dokter')->group(function ($router) {
