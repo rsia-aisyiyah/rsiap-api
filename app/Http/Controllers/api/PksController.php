3f43aba95233e097ec439f489676162dba0429ef
halimkun <ffaisalhalim@gmail.com>
2023-12-09 10:17:37 +0700

    Add file upload functionality to PKS update

--- app/Http/Controllers/api/PksController.php
+++ app/Http/Controllers/api/PksController.php
@@ -104,6 +104,7 @@ class PksController extends Controller
             'judul' => 'required',
             'pj' => 'required',
             'tanggal_awal' => 'required',
+            'file' => 'mimes:pdf|max:2048',
         ];
 
         $validator = \Illuminate\Support\Facades\Validator::make($request->all(), $rules);
@@ -112,7 +113,62 @@ class PksController extends Controller
             return isFail($validator->errors(), 422);
         }
 
-        $pks->update($request->all());
+        // if file existss
+        if ($request->hasFile('file')) {
+            $file = $request->file('file');
+            $file_name = strtotime(now()) . '-' . str_replace([' ', '_'], '-', $file->getClientOriginalName());
+            
+            // move 
+            $st = new \Illuminate\Support\Facades\Storage();
+            // if directory not exists create it
+            if (!$st::disk('sftp')->exists(env('DOCUMENT_PKS_SAVE_LOCATION'))) {
+                $st::disk('sftp')->makeDirectory(env('DOCUMENT_PKS_SAVE_LOCATION'));
+            }
+            // move file
+            $st::disk('sftp')->put(env('DOCUMENT_PKS_SAVE_LOCATION') . $file_name, file_get_contents($file));
+            // final data
+            $final_data = [
+                'no_pks_internal' => $request->no_pks_internal,
+                'no_pks_eksternal' => $request->no_pks_eksternal ?? "",
+                'judul' => $request->judul,
+                'tanggal_awal' => $request->tanggal_awal,
+                'tanggal_akhir' => $request->tanggal_akhir ?? "0000-00-00",
+                'berkas' => $file_name,
+                'pj' => $request->pj,
+            ];
+        } else {
+            $final_data = [
+                'no_pks_internal' => $request->no_pks_internal,
+                'no_pks_eksternal' => $request->no_pks_eksternal ?? "",
+                'judul' => $request->judul,
+                'tanggal_awal' => $request->tanggal_awal,
+                'tanggal_akhir' => $request->tanggal_akhir ?? "0000-00-00",
+                'pj' => $request->pj,
+            ];
+        }
+        
+        // old berkas
+        $old_berkas = $pks->berkas;
+
+        //  try update date using database transaction
+        \Illuminate\Support\Facades\DB::beginTransaction();
+        try {
+            // get old berkas and if new data has berkas and finished update delete old berkas
+            $pks->update($final_data);
+            \Illuminate\Support\Facades\DB::commit();
+        } catch (\Throwable $th) {
+            \Illuminate\Support\Facades\DB::rollback();
+            return isFail($th->getMessage(), 500);
+        }
+        
+        // delete old berkas
+        if ($request->hasFile('file')) {
+            $st = new \Illuminate\Support\Facades\Storage();
+            if ($st::disk('sftp')->exists(env('DOCUMENT_PKS_SAVE_LOCATION') . $old_berkas)) {
+                $st::disk('sftp')->delete(env('DOCUMENT_PKS_SAVE_LOCATION') . $old_berkas);
+            }
+        }
+
         return isSuccess($pks, 'Data PKS berhasil diupdate');
     }
 
