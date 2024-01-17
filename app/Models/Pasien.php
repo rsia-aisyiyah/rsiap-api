ef18df3b8f7861049ed1bce0eb3507bbd0196ec8
halimkun <ffaisalhalim@gmail.com>
2024-01-17 11:55:06 +0700

    Add API endpoints for retrieving pasien data and metrics

--- app/Models/Pasien.php
+++ app/Models/Pasien.php
@@ -4,16 +4,47 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class Pasien extends Model
+class Pasien extends Model implements JWTSubject
 {
     use HasFactory;
 
     protected $table = 'pasien';
-    protected $hidden = ['no_rkm_medis', 'no_ktp', 'no_peserta'];
 
+    protected $primaryKey = 'no_rkm_medis';
+
+    protected $hidden = ['no_rkm_medis', 'no_ktp', 'no_peserta'];
+    
+    protected $guarded = [];
+
+    public $timestamps = false;
+
+
+    
     public function regPeriksa()
     {
         return $this->hasMany(RegPeriksa::class, 'no_rkm_medis', 'no_rkm_medis');
+    }
+
+    /**
+	 * Get the identifier that will be stored in the subject claim of the JWT.
+	 * @return mixed
+	 */
+	public function getJWTIdentifier() {
+        return $this->getKey();
+	}
+	
+	/**
+	 * Return a key value array, containing any custom claims to be added to the JWT.
+	 * @return array
+	 */
+	public function getJWTCustomClaims() {
+        return [];
+	}
+
+    function spesialis()
+    {
+        return $this->belongsTo(Spesialis::class, 'kd_sps', 'kd_sps');
     }
 }
