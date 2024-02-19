0289bd9440c7b57da0afc043d9f0ccf2cbcee0e0
halimkun <ffaisalhalim@gmail.com>
2024-02-19 08:26:52 +0700

    Refactor print/undangan_internal.blade.php

--- resources/views/print/undangan_internal.blade.php
+++ resources/views/print/undangan_internal.blade.php
@@ -24,25 +24,22 @@
   <tr>
     <th style="width: 300px;">Lampiran</th>
     <td style="width: 50px;">:</td>
-    <td>-</td>
+    <td>
+      <?= count($penerima) <= 7 ? '-' : '1 (satu) berkas' ?>
+    </td>
   </tr>
 </table>
 
 <div class="mb-2">
   <span class="p-0 m-0">Kepada Yth.</span>
-  <table class="table table-borderless table-sm mb-0">
-    <tr>
-      @foreach ($penerima as $key => $val)
-      <td class="p-0 m-0" style="width: 50px;">
-        <ol class="mb-0 pb-0">
-          @foreach ($val as $v)
-            <li>{{ $v->pegawai->nama }}</li>
-          @endforeach
-        </ol>
-      </td>
-      @endforeach
-    </tr>
-  </table>
+  @if (count($penerima) <= 7) <ol class="mr-2" style="list-style-type: decimal; padding-inline-start: 20px;">
+    @foreach ($penerima as $key => $item)
+    <li>{{ $item->pegawai->nama }}</li>
+    @endforeach
+    </ol>
+    @else
+    <p>Nama-nama terlampir</p>
+    @endif
 </div>
 
 <p class="mb-3">
@@ -95,10 +92,130 @@
     <td class="p-0 m-0">
       <p class="mb-0 pb-0">Pekalongan, {{ \Carbon\Carbon::now()->isoFormat('D MMMM Y') }}</p>
       {{-- $undangan->pegawai_detail->jenjang_jabatan->nama is capital make is cammel case--}}
-      <p class="mb-0 pb-0" style="font-weight: bold; text-transform: capitalize;">{{ strtolower($undangan->pegawai_detail->jenjang_jabatan->nama) }}</p>
-      <br /><br /><br />
+      <p class="mb-0 pb-0" style="font-weight: bold; text-transform: capitalize;">{{
+        strtolower($undangan->pegawai_detail->jenjang_jabatan->nama) }}</p>
+      <br /><br /><br /><br />
       <p class="mb-0 pb-0" style="font-weight: bold">{{ $undangan->pegawai_detail->nama }}</p>
     </td>
   </tr>
 </table>
+
+@if (count($penerima) > 7)
+{{-- break for penerima --}}
+<div class="break-page"></div>
+
+<h5 class="text-center" style="font-family: 'Times New Roman', Times, serif; font-weight: bold;">
+  Daftar Nama Terundang
+</h5>
+
+<div class="mt-4">
+  <table class="table table-sm">
+    <tr>
+      <th>No.</th>
+      <th>Nama</th>
+      <th>Bidang</th>
+      <th>Jabatan</th>
+    </tr>
+    <tbody>
+      @foreach ($penerima as $key => $item)
+      <tr>
+        <td>{{ $key + 1 }}</td>
+        <td>{{ $item->pegawai->nama }}</td>
+        <td>{{ $item->pegawai->bidang }}</td>
+        <td>{{ $item->pegawai->jbtn }}</td>
+      </tr>
+      @endforeach
+    </tbody>
+  </table>
+</div>
+@endif
+
+
+{{-- Daftar Hadir --}}
+<div class="break-page"></div>
+<?php $maxRow = 25 ?>
+
+<h5 class="text-center" style="font-family: 'Times New Roman', Times, serif; font-weight: bold;">
+  <u>DAFTAR HADIR</u>
+</h5>
+
+<table>
+  <tr>
+    <th style="width: 4cm;">Hari / Tanggal</th>
+    <td>: {{ \Carbon\Carbon::parse($undangan->tanggal)->isoFormat('dddd, D MMMM Y') }}</td>
+  </tr>
+  <tr>
+    <th style="width: 4cm;">Jam</th>
+    <td>: {{ \Carbon\Carbon::parse($undangan->tanggal)->isoFormat('HH:mm') }} WIB s/d selesai</td>
+  </tr>
+  <tr>
+    <th style="width: 4cm;">Agenda</th>
+    <td>: {{ $undangan->perihal }}</td>
+  </tr>
+</table>
+
+<div class="mt-0">
+  <div class="text-right">
+    <small class="text-muted" style="color: #6c757d !important; font-size: 0.7rem !important;">
+      Generated automatically by system - {{ \Carbon\Carbon::now()->isoFormat('D MMMM Y HH:mm') }}
+    </small>
+  </div>
+  <table class="table table-bordered table-sm">
+    <tr>
+      <th>No.</th>
+      <th>Nama</th>
+      <th>Jabatan</th>
+      <th style="width:4cm !important;">TTD</th>
+    </tr>
+    <tbody>
+      @foreach ($penerima as $key => $item)
+      <tr>
+        <td class="text-center">{{ $key + 1 }}.</td>
+        <td>{{ $item->pegawai->nama }}</td>
+        {{-- if in jbtn contain koordinator change to koor --}}
+        <td>{{ str_contains(strtolower($item->pegawai->jbtn), 'koordinator') ? ucfirst(str_replace('koordinator', 'koor', strtolower($item->pegawai->jbtn))) : strtolower($item->pegawai->jbtn) }}</td>
+        <td>
+          @if ($key % 2 == 0)
+            <div class="">{{ $key + 1 }}.</div>
+          @else
+            <div class="text-center">{{ $key + 1 }}.</div>
+          @endif
+        </td>
+      </tr>
+      @endforeach
+
+      {{-- if count penerima < maxRow loop blank  --}}
+      @if (count($penerima) < $maxRow)
+        @for ($i = 0; $i < $maxRow - count($penerima); $i++) 
+          <tr>
+            <td class="text-center">
+              {{-- lanjutkan penomoran jika tr diatas sudah 20 maka disini dimulai dari 21 --}}
+              {{ $i + count($penerima) + 1 }}.
+            </td>
+            <td><div class="my-3"></div></td>
+            <td><div class="my-3"></div></td>
+            <td>
+              @if (($i + count($penerima)) % 2 == 0)
+                <div class="">{{ $i + count($penerima) + 1 }}.</div>
+              @else
+                <div class="text-center">{{ $i + count($penerima) + 1 }}.</div>
+              @endif
+            </td>
+          </tr>
+        @endfor
+      @elseif (count($penerima) > $maxRow)
+        @for ($i = 0; $i < $maxRow - (count($penerima) % $maxRow); $i++) 
+          <tr>
+            <td><div class="my-3"></div></td>
+            <td><div class="my-3"></div></td>
+            <td><div class="my-3"></div></td>
+            <td><div class="my-3"></div></td>
+          </tr>
+        @endfor
+      @endif
+    </tbody>
+  </table>
+</div>
+
+
 @endsection
