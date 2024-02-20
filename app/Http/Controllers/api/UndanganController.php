808b05fc7cd9dc2bd275bcbdf03934bf695478d6
halimkun <ffaisalhalim@gmail.com>
2024-02-20 09:03:08 +0700

    Refactor UndanganController index method to include keyword search and datatables support

--- app/Http/Controllers/api/UndanganController.php
+++ app/Http/Controllers/api/UndanganController.php
@@ -7,21 +7,45 @@ use Illuminate\Http\Request;
 
 class UndanganController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $data = \App\Models\RsiaSuratInternalPenerima::select("*")
             ->with(['surat' => function ($q) {
                 $q->with(['penanggung_jawab' => function ($q) {
                     $q->select('nik', 'nama');
                 }]);
-            }, 'notulen' => function($q) {
+            }, 'notulen' => function ($q) {
                 $q->select('no_surat', 'notulis_nik', 'created_at')->with(['notulis' => function ($q) {
                     $q->select('nik', 'nama');
                 }]);
             }])
             ->orderBy('no_surat', 'DESC')
-            ->groupBy('no_surat')
-            ->paginate(env('PER_PAGE', 10));
+            ->groupBy('no_surat');
+
+        if ($request->keyword) {
+            $data = $data->where('no_surat', 'like', '%' . $request->keyword . '%')
+                ->orWhereHas('surat', function ($q) use ($request) {
+                    $q->where('perihal', 'like', '%' . $request->keyword . '%')->orWhereHas('penanggung_jawab', function ($q) use ($request) {
+                        $q->where('nama', 'like', '%' . $request->keyword . '%');
+                    });
+                })
+                ->orWhereHas('notulen', function ($q) use ($request) {
+                    $q->whereHas('notulis', function ($q) use ($request) {
+                        $q->where('nama', 'like', '%' . $request->keyword . '%');
+                    });
+                });
+        }
+
+        if ($request->datatables) {
+            if ($request->datatables == 1 || $request->datatables == true || $request->datatables == 'true') {
+                $data = $data->get();
+                return \Yajra\DataTables\DataTables::of($data)->make(true);
+            } else {
+                $data = $data->paginate(env('PER_PAGE', 10));
+            }
+        } else {
+            $data = $data->paginate(env('PER_PAGE', 10));
+        }
 
         return isSuccess($data, "Berhasil mendapatkan data");
     }
@@ -30,7 +54,7 @@ class UndanganController extends Controller
     {
         $nip = $request->payload['sub'];
         $data = \App\Models\RsiaSuratInternalPenerima::select("*")
-            ->with('surat')   
+            ->with('surat')
             ->where('penerima', $nip)
             ->orderBy('no_surat', 'DESC')
             ->paginate(env('PER_PAGE', 10));
