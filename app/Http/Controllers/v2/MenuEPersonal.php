9e3146e3e1ba79e2b6a91f062a9c1d5bc99b3fbe
halimkun <ffaisalhalim@gmail.com>
2024-03-04 10:58:49 +0700

    Update MenuEPersonal controller to use 'nik' instead of 'dep' for filtering

--- app/Http/Controllers/v2/MenuEPersonal.php
+++ app/Http/Controllers/v2/MenuEPersonal.php
@@ -14,7 +14,7 @@ class MenuEPersonal extends Controller
      */
     public function index(Request $request)
     {
-        $set_data = \App\Models\SetMenuEPersonal::select('menu_id')->where('dep_id', $request->dep)->get();
+        $set_data = \App\Models\SetMenuEPersonal::select('menu_id')->where('nik', $request->nik)->get();
         $data = \App\Models\RsiaMasterMenuEPersonal::whereIn('id', $set_data)->orderBy('urutan', 'ASC')->get()->groupBy('group');
         return new \App\Http\Resources\Collection\MenuEPersonalCollection($data);
     }
