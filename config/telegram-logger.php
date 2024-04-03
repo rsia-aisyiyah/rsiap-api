5200b1bd607c98ff907301ea42f3201cd9933b8f
halimkun <ffaisalhalim@gmail.com>
2024-04-03 08:05:17 +0700

    Add Laravel Telegram Logging package and Orion package***

---
+++ config/telegram-logger.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    // Telegram logger bot token
+    'token' => env('TELEGRAM_LOGGER_BOT_TOKEN'),
+
+    // Telegram chat id
+    'chat_id' => env('TELEGRAM_LOGGER_CHAT_ID'),
+
+    // Blade Template to use formatting logs
+    'template' => env('TELEGRAM_LOGGER_TEMPLATE', 'laravel-telegram-logging::standard'),
+
+    // Proxy server
+    'proxy' => env('TELEGRAM_LOGGER_PROXY', ''),
+
+    // Telegram API host without trailling slash
+    'api_host' => env('TELEGRAM_LOGGER_API_HOST', 'https://api.telegram.org'),
+
+    // Telegram sendMessage options: https://core.telegram.org/bots/api#sendmessage
+    'options' => [
+        'message_thread_id' => env('TELEGRAM_LOGGER_MESSAGE_THREAD_ID', null)
+        // 'parse_mode' => 'html',
+        // 'disable_web_page_preview' => true,
+        // 'disable_notification' => false
+    ]
+];
